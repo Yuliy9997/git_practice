b8296daa2d8111add94a1295f53b804401147362
Julie9997 <ula.9997@mail.ru>
2023-08-21 10:41:17 +0300

    Delete function

--- some_file.py
+++ some_file.py
@@ -1,17 +1,5 @@
 import math
 
-N = int(input('N = '))
-
-divide = []
-
-for i in range(2, N + 1):
-    while N % i == 0:
-        divide.append(i)
-        N //= i
-
-print(*divide, sep=' * ')
-
-
 n = int(input('Введите количесиво знаков послле запятой: '))
  
 pi = math.pi
